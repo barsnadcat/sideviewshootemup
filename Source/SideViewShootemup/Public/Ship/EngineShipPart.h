1632e4efcf78ed911be2dda10d83ca3c18f779db
barsandcat <barsandcat@gmail.com>
2024-03-05 11:24:41 +0100

    Specific parts

---
+++ Source/SideViewShootemup/Public/Ship/EngineShipPart.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Ship/ShipPart.h"
+#include "EngineShipPart.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SIDEVIEWSHOOTEMUP_API AEngineShipPart : public AShipPart
+{
+	GENERATED_BODY()
+	
+};
