0e4065f5af9f5218f35c9ee103e0a70fac642d07
barsandcat <barsandcat@gmail.com>
2024-05-11 15:25:23 +0200

    Clean up AddForceAtLocation - instead of searching truogh all top level clusters, just apply force to our cluster. Put waking togeteher with addforce, istead of calling two separte commands.

--- Source/SideViewShootemup/Private/Ship/MyClusterUnionComponent.cpp
+++ Source/SideViewShootemup/Private/Ship/MyClusterUnionComponent.cpp
@@ -2,41 +2,27 @@
 
 #include "PhysicsProxy/ClusterUnionPhysicsProxy.h"
 
-void UMyClusterUnionComponent::AddForceAtLocation(FVector Force, FVector WorldLocation, FName BoneName)
+void UMyClusterUnionComponent::AddForceAtLocation(FVector force, FVector worldLocation, FName boneName)
 {
-    if (auto* proxy = GetPhysicsProxy())
+    auto* proxy = GetPhysicsProxy();
+    if (!proxy)
     {
-        if (Chaos::FPhysicsSolver* RBDSolver = proxy->GetSolver<Chaos::FPhysicsSolver>())
-        {
-            WakeAllRigidBodies();
-            RBDSolver->EnqueueCommandImmediate([proxy, Force, WorldLocation, RBDSolver]()
-                {
-				Chaos::FReal ClosestDistanceSquared = TNumericLimits<Chaos::FReal>::Max();
-				Chaos::FPBDRigidClusteredParticleHandle* ClosestHandle = nullptr;
-
-				Chaos::FRigidClustering& Clustering = RBDSolver->GetEvolution()->GetRigidClustering();
-				for (Chaos::FPBDRigidClusteredParticleHandle* ClusteredHandle : Clustering.GetTopLevelClusterParents())
-				{
-                    if (ClusteredHandle && ClusteredHandle->PhysicsProxy() == proxy)
-					{
-						if (ClusteredHandle->IsDynamic())
-						{
-							const Chaos::FReal DistanceSquared = (WorldLocation - ClusteredHandle->GetX()).SquaredLength();
-							if (DistanceSquared < ClosestDistanceSquared)
-							{
-								ClosestDistanceSquared = DistanceSquared;
-								ClosestHandle = ClusteredHandle;
-							}
-						}
-					}
-				}
-				if (ClosestHandle)
-				{
-					const Chaos::FVec3 WorldCOM = Chaos::FParticleUtilitiesXR::GetCoMWorldPosition(ClosestHandle);
-					const Chaos::FVec3 WorldTorque = Chaos::FVec3::CrossProduct(WorldLocation - WorldCOM, Force);
-					ClosestHandle->AddForce(Force);
-					ClosestHandle->AddTorque(WorldTorque);
-				} });
-        }
+        return;
     }
+    Chaos::FPhysicsSolver* solver = proxy->GetSolver<Chaos::FPhysicsSolver>();
+    Chaos::FPBDRigidClusteredParticleHandle* particleHandle = proxy->GetParticle_Internal();
+    if (!particleHandle || !solver)
+    {
+        return;
+    }
+
+    solver->EnqueueCommandImmediate(
+        [solver, particleHandle, force, worldLocation]()
+        {
+            const Chaos::FVec3 worldCOM = Chaos::FParticleUtilitiesXR::GetCoMWorldPosition(particleHandle);
+            const Chaos::FVec3 worldTorque = Chaos::FVec3::CrossProduct(worldLocation - worldCOM, force);
+            particleHandle->AddForce(force);
+            particleHandle->AddTorque(worldTorque);
+            solver->GetEvolution()->WakeParticle(particleHandle);
+        });
 }
